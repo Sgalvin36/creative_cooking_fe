9666178f112850813de2cfbc8a8e57d851224c2b
Shane Galvin <Sgalvin36@gmail.com>
2025-04-02 09:46:30 -0600

    Added about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>This is about page</p>
+            <footer>
+                <a href="/" rel="prev">Back</a>
+            </footer>
+        </div>
+    )
+}
