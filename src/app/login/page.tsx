9384cd679b92f4a1039510f19d1d432c09bdd798
Shane Galvin <Sgalvin36@gmail.com>
2025-04-02 10:20:09 -0600

    Add dynamic routing for user profiles

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Login() {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            <p>This is my login page</p>
+            <footer>
+                <a href="/" rel="prev">Back</a>
+            </footer>
+        </div>
+    )
+}
