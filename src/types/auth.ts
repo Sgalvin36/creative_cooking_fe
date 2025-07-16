fb2d949d1fd3bbfcbc07c1b40118c374a007e8d1
Shane Galvin <Sgalvin36@gmail.com>
2025-07-16 10:56:37 -0600

    Updated SiteUser type to hold cookbook info

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -4,9 +4,11 @@ export type SiteUser = {
   id: number;
   first_name: string;
   last_name: string;
-  // user_name: string;
+  user_name: string;
   email: string;
   slug: string;
+  cookbook_count: number;
+  primary_cookbook_id: number;
 };
 
 export type AuthContextType = {
